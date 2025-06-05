0e4105c4f3244975b02adc91c157204ffcafd720
Gleb Chetvertak <glebsterchetvertak@gmail.com>
2025-06-06 01:12:50 +0300

    fix base url

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/aiscreen-test/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
