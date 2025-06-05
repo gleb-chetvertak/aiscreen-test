9447186e720b5a1f0919317b7df4af66cf62b565
Gleb Chetvertak <glebsterchetvertak@gmail.com>
2025-06-06 01:39:15 +0300

    adding dist

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/gleb-chetvertak/aiscreen-test/',
+  base: '/aiscreen-test/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
